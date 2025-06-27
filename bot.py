a7652b08c1e296da1a083289166f38b8bcac3904
MiaoLala <111435584+MiaoLala@users.noreply.github.com>
2025-06-27 13:53:35 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,6 +26,10 @@ class DummyHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"OK")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
 def run_dummy_server():
     port = int(os.environ.get("PORT", 5000))
     server = HTTPServer(("0.0.0.0", port), DummyHandler)
